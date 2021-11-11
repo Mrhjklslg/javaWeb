1b2ec7dd3f047f0f3ca45436d9a47d54496d5a0c
hj <2207392187@qq.com>
2021-11-11 19:00:42 +0800

    第四次提交

--- src/resource/webAPP.sql
+++ src/resource/webAPP.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `user` (
     `id`  int(11) NOT NULL AUTO_INCREMENT ,
-    `username`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+    `name`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
     `gender`  varchar(5) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
     `age`  int(11) NULL DEFAULT NULL ,
     `address`  varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
